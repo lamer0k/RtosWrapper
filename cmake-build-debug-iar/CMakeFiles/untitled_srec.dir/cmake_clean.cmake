d72edf5a347c879712af1b758641b8854b47d829
lamer0k <sergey.kolody@gmail.com>
2022-02-26 10:27:09 +0500

    Update README.adoc

--- cmake-build-debug-iar/CMakeFiles/untitled_srec.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/untitled_srec"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/untitled_srec.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
